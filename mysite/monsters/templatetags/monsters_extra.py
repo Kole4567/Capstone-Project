031a3518ba04a874f8c2bd56a7d5f453af954b6c
root <root@Koltons-Laptop.localdomain>
2026-02-17 19:55:23 -0600

    Added some more images

--- mysite/monsters/templatetags/monsters_extra.py
+++ mysite/monsters/templatetags/monsters_extra.py
@@ -6,3 +6,25 @@ register = template.Library()
 def monster_icon(monster_name):
     filename = monster_name.replace(" ", "_").replace("-", "_")
     return f"images/monsters_icon/MHW_{filename}_Icon.webp"
+
+@register.filter
+def weapon_icon(weapon_type):
+    # Normalize separators
+    normalized = (
+        weapon_type
+        .replace("-", " ")
+        .replace("&", " and ")
+    )
+
+    parts = normalized.split()
+
+    filename_parts = []
+    for word in parts:
+        if word.lower() == "and":
+            filename_parts.append("and")
+        else:
+            filename_parts.append(word.capitalize())
+
+    filename = "_".join(filename_parts)
+
+    return f"images/weapons_icon/{filename}_Icon_White.webp"
