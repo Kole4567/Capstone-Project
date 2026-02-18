a32774f804549750526f79e125738ab147bec19c
root <root@Koltons-Laptop.localdomain>
2026-02-17 18:06:07 -0600

    Recovered project data and monster icon logic

--- mysite/armors/views.py
+++ mysite/armors/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from MonsterHunterWorld.models import Armor
 
 # This name MUST match what is in your weapons/urls.py
 def armors_index(request):
-    return render(request, 'armors.html')
+    # Fetch all weapons stored in YOUR internal database
+    # This uses your stable API contracts instead of the mhw-db structure
+    armors = Armor.objects.prefetch_related('armor_skills__skill').all().order_by('rarity')
+    
+    return render(request, 'armors.html', {
+        'armors': armors
+    })
