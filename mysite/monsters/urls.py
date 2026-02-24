5df8d1d67a5daaf70c1aae6dbf2ceb3f988c202a
root <root@Koltons-Laptop.localdomain>
2026-02-23 23:27:56 -0600

    Some stuff broke and had to fix it

--- mysite/monsters/urls.py
+++ mysite/monsters/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.monsters_index, name='monsters_index'), # This leads to /monsters
+    path('monster/<int:monster_id>/build/', views.monster_detail, name='monster_build'),
 ]
--- mysite/monsters/views.py
+++ mysite/monsters/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from MonsterHunterWorld.models import Monster
+from MonsterHunterWorld.build_logic import best_build_fast 
 
-# This name MUST match what is in your weapons/urls.py
 def monsters_index(request):
-    # Fetch all weapons stored in YOUR internal database
-    # This uses your stable API contracts instead of the mhw-db structure
+    # Fetch all weapons stored in internal database
     monsters = Monster.objects.all().order_by('name')
     
     return render(request, 'monsters.html', {
         'monsters': monsters
+    })
+
+
+
+def monster_detail(request, monster_id):
+    monster = Monster.objects.get(id=monster_id)
+    
+    recommendation = best_build_fast(monster) 
+    
+    return render(request, 'build_recommendation.html', {
+        'monster': monster,
+        'build': recommendation
     })
